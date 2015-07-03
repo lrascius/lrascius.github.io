44d6161ee13cf62ca29264d456bcca240fcbdfda
Lukas Rascius <lrascius93@gmail.com>
2015-07-02 21:55:05 -0400

    Adding smooth scroll

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,14 @@
 /*global $ */
 
 $(document).ready(function () {
-    
+    /* Initialize the smooth scroll */
+    smoothScroll.init({
+        speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+        easing: 'easeInCubic' // Easing pattern to use
+    });
     /* Navigation JS : Removes active class from current li and adds the active class to the new current li */
     $("li").click(function() {
          $("li.active").removeClass("active");
          $(this).addClass("active");  
-    });
-    
-    
+    });  
 });
