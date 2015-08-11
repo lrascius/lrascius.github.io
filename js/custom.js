aaf7a2f098da1c26242db967df0ca59d68b8840d
Lukas Rascius <lrascius93@gmail.com>
2015-08-10 20:25:14 -0400

    Adding some wow animation

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,9 @@ $(document).ready(function () {
         speed: 1000, // Integer. How fast to complete the scroll in milliseconds
         easing: 'easeInCubic' // Easing pattern to use
     });
+    /* Initialize WOW animations */
+    new WOW().init();
+    
     /* Navigation JS : Removes active class from current li and adds the active class to the new current li */
     $("li").click(function() {
          $("li.active").removeClass("active");
